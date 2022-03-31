7a3633beeb782acd964280b6902c4c1fda2a40db
Diego A. Suaza R <diegos9307@gmail.com>
2022-03-31 11:41:33 -0500

    renderizado condicional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      check: false,
+    };
+    this.handleChecked = this.handleChecked.bind(this);
+  }
+
+  handleChecked(e) {
+    if (this.state.check === false) {
+      this.setState({
+        check: true,
+      });
+    } else {
+      this.setState({
+        check: false,
+      });
+    }
+  }
+
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label>
+          <input type="checkbox" onChange={this.handleChecked} /> Mostrar
+          información importante
+        </label>
+        {this.state.check ? (
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        ) : null}
       </div>
     );
   }
